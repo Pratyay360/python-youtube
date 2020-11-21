7ad7246e7060778aa3174929a0d57ff82afb5a10
pratyay360 <pratyaymustafi@gmail.com>
2020-11-21 12:50:28 +0530

    Create youtubethumbnail.py

---
+++ youtubethumbnail.py
@@ -0,0 +1,10 @@
+import wget
+#ascii_banner = pyfiglet.figlet_format("YouTube Thumbnail Downloader")
+ascii_banner = "YouTube Thumbnail Downloader"
+print(ascii_banner)
+url= input("Enter YouTube URL : ")
+id = url.split("=",1)[1]
+thumbnailurl= 'https://img.youtube.com/vi/'+id + '/maxresdefault.jpg'
+print("Downloading Thumbnail...")
+thumbnail = wget.download(thumbnailurl)
+print("\n" + "Thumbnail Downloaded")
